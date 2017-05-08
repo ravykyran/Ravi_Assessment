b28fd401bf3b3a840b4c6a6a6e555c2b2323289c
ravykyran <ravykyran@gmail.com>
2017-05-08 21:11:24 +0200

    Design Completed

--- Bank/src/main/java/com/wipro/bank/assessment/App.java
+++ Bank/src/main/java/com/wipro/bank/assessment/App.java
@@ -1,13 +1,34 @@
 package com.wipro.bank.assessment;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.wipro.bank.entity.Account;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+	public static final List<Account> accounts;
+		
+	public Account getAccountDetailsByID(int id){
+		
+		Account account = null;
+		
+		
+		
+		
+		
+		return account;		
+	}
+	
+	
+	public List<Account> getAccountDetailsByBalance(double amount){
+		List<Account> foundAccount = new ArrayList<Account>();
+		
+		return foundAccount;
+	}
+	
 }
